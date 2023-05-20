8b5cd59f98557d1a5b2c2a0595cf4a9aee2fed78
Julian954 <sanchezjulian687@gmail.com>
2023-05-19 20:21:26 -0600

    PedidosPage

--- Controller/Login.php
+++ Controller/Login.php
@@ -1,16 +1,18 @@
 <?php
-    class Login extends Controllers{
+    class Login extends Controllers
+    {
         public function __construct()
         {
-        session_start();
-        if (!empty($_SESSION['activo'])) {
-            header("location: " . base_url()."Inicio/Home");
-        }
+         session_start();
+         if (!empty($_SESSION['activo'])) 
+            {
+                header("location: " . base_url()."Inicio/Home");
+            }
             parent::__construct();
         }
         public function login()
         {
             $this->views->getView($this, "login");
         }
-}
+    }
 ?>
