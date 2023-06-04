a12c4fba59005221cda482ea45f34e83c73092ce
Jordan (Laptop) <miguel20010807@gmail.com>
2023-06-03 21:41:25 -0600

    Cambios Varios, sigo en proceso

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
     const BASE_URL = "http://localhost/IMSS/";
-    const HOST = "localhost:3308";
-    const BD = "imss(1)";
+    const HOST = "localhost";
+    const BD = "imss";
     const DB_USER = "root";
     const PASS = "";
     const CHARSET = "charset=utf8";
--- Views/Contratos/General.php
+++ Views/Contratos/General.php
@@ -34,26 +34,25 @@
                               </div><!--//row-->
                             </div><!--//app-card-header-->
                             <div class="app-card-body p-4">
-                              <div style="">
+                              <div>
                                 <span>
-                                  <svg width="0.5em" height="0.5em"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="red">
+                                  <svg width="1em" height="1em"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="red">
                                     <path d="M396.8 352h22.4c6.4 0 12.8-6.4 12.8-12.8V108.8c0-6.4-6.4-12.8-12.8-12.8h-22.4c-6.4 0-12.8 6.4-12.8 12.8v230.4c0 6.4 6.4 12.8 12.8 12.8zm-192 0h22.4c6.4 0 12.8-6.4 12.8-12.8V140.8c0-6.4-6.4-12.8-12.8-12.8h-22.4c-6.4 0-12.8 6.4-12.8 12.8v198.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h22.4c6.4 0 12.8-6.4 12.8-12.8V204.8c0-6.4-6.4-12.8-12.8-12.8h-22.4c-6.4 0-12.8 6.4-12.8 12.8v134.4c0 6.4 6.4 12.8 12.8 12.8zM496 400H48V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-16c0-8.84-7.16-16-16-16zm-387.2-48h22.4c6.4 0 12.8-6.4 12.8-12.8v-70.4c0-6.4-6.4-12.8-12.8-12.8h-22.4c-6.4 0-12.8 6.4-12.8 12.8v70.4c0 6.4 6.4 12.8 12.8 12.8z"/>
                                   </svg>
-                                  &nbsp&nbsp
+
                                   <span style="font-weight: bold;">Estadísticas de la Oficina</span>
                                 </span>
                               </div>
                             <div class="row g-4 mb-4" style="padding-top:10px; font-size:14px;">
                               <div class="col-12 col-lg-6" style="color:#000000;">
 
-  <div>
-    <?php $tot = $data3[0]['total']; ?>
-    <span style="font-weight:bold;"></span><span style="">Total de Instrumentos:</span>&nbsp<strong><span style="font-weight:bold; text-decoration:solid underline #5B99EA 2px;"> <?php echo $tot; ?> </strong> </span>
-    <?php $max = $data3[0]['maximo']; ?>                                      
-    </span>&nbsp<span>a un ascendente de :</span>&nbsp<span style="font-weight:bold;">$<?php echo number_format($max, 2); ?>
-    </span>
-  </div>
-<div>
+                            <div>
+                              <span style="font-weight:bold;">Total de Instrumentos:</span>&nbsp<span style="font-weight:bold; text-decoration:solid underline #5B99EA 2px;"><?php echo $data3[0]['total']; ?></span>                       
+                              </span>&nbsp<span>a un ascendente de: </span>&nbsp<span style="font-weight:bold;">$<?php echo number_format($data3[0]['maximo'], 2); ?></span>
+                            </div>
+                          <div>
+
+
   <span style="text-decoration:solid underline #5B99EA 2px;">
   <?php $conV = $data4[1]['cont']; ?>
   <?php echo $conV; ?>
@@ -407,113 +406,7 @@ $pTotalDatoBarra7 = ($pTotalContratosAreaInf != 0) ? ($pTotalContratosAreaIN / $
 
         </div>
       </div>
-	    <div class="app-content pt-3 p-md-3 p-lg-4">
-		    <div class="container-xl">
-			    <div class="row g-4">
-				    <div class="col-6 col-md-4 col-xl-3 col-xxl-2">
-					    <div class="app-card app-card-doc shadow-sm h-100">
 
-                <div class="app-card-thumb-holder p-3">
-							    <span class="icon-holder">
-	                                <i class="fas fa-file-pdf pdf-file"></i>
-	                            </span>
-	                             <a class="app-card-link-mask" href="#file-link"></a>
-						    </div>
-						    <div class="app-card-body p-3 has-card-actions">
-
-							    <h4 class="app-doc-title truncate mb-0"><a href="#file-link">Descarga/a></h4>
-							    <div class="app-doc-meta">
-								    <ul class="list-unstyled mb-0">
-									    <li><span class="text-muted">Formato:</span> PDF</li>
-									    <li><span class="text-muted">Peso (promedio):</span> 3MB</li>
-									    <li><span class="text-muted">Vigencia:</span> 2021o</li>
-								    </ul>
-							    </div><!--//app-doc-meta-->
-
-							    <div class="app-card-actions">
-								    <div class="dropdown">
-									    <div class="dropdown-toggle no-toggle-arrow" data-toggle="dropdown" aria-expanded="false">
-										    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-			  <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-			</svg>
-									    </div><!--//dropdown-toggle-->
-									    <ul class="dropdown-menu dropdown-menu-right">
-										    <li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z"/>
-	  <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-	</svg>View</a></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-	</svg>Edit</a></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-	  <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-	</svg>Download</a></li>
-	                                        <li><hr class="dropdown-divider"></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-	  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-	</svg>Delete</a></li>
-										</ul>
-								    </div><!--//dropdown-->
-						        </div><!--//app-card-actions-->
-						    </div><!--//app-card-body-->
-						</div><!--//app-card-->
-				    </div><!--//col-->
-				    <div class="col-6 col-md-4 col-xl-3 col-xxl-2">
-              <div class="app-card app-card-doc shadow-sm h-100">
-						    <div class="app-card-thumb-holder p-3">
-							    <span class="icon-holder">
-	                                <i class="fas fa-file-excel excel-file"></i>
-	                            </span>
-	                             <a class="app-card-link-mask" href="#file-link"></a>
-						    </div>
-						    <div class="app-card-body p-3 has-card-actions">
-
-							    <h4 class="app-doc-title truncate mb-0"><a href="#file-link">Relación</a></h4>
-							    <div class="app-doc-meta">
-								    <ul class="list-unstyled mb-0">
-									    <li><span class="text-muted">Formato:</span> Macro Excel</li>
-									    <li><span class="text-muted">Peso:</span> 2 MB</li>
-									    <li><span class="text-muted">Actualización:</span> 2 de maro 2021</li>
-								    </ul>
-							    </div><!--//app-doc-meta-->
-
-							    <div class="app-card-actions">
-								    <div class="dropdown">
-									    <div class="dropdown-toggle no-toggle-arrow" data-toggle="dropdown" aria-expanded="false">
-										    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-			  <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-			</svg>
-									    </div><!--//dropdown-toggle-->
-									    <ul class="dropdown-menu dropdown-menu-right">
-										    <li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z"/>
-	  <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-	</svg>View</a></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-	</svg>Edit</a></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path fill-rule="evenodd" d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-	  <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-	</svg>Download</a></li>
-	                                        <li><hr class="dropdown-divider"></li>
-											<li><a class="dropdown-item" href="#"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-	  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-	  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-	</svg>Delete</a></li>
-										</ul>
-								    </div><!--//dropdown-->
-						        </div><!--//app-card-actions-->
-
-						    </div><!--//app-card-body-->
-
-						</div><!--//app-card-->
-				    </div><!--//col-->
-			    </div><!--//row-->
-		    </div><!--//container-fluid-->
-	    </div><!--//app-content-->
 
               </div><!--//app-card-body-->
             </div>
@@ -521,18 +414,6 @@ $pTotalDatoBarra7 = ($pTotalContratosAreaInf != 0) ? ($pTotalContratosAreaIN / $
         </div>
       </div>
     </div><!--//app-wrapper-->
-    <!-- Javascript -->
-    <script src="assets/plugins/popper.min.js"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-
-    <!-- Page Specific JS -->
-    <script src="assets/js/app.js"></script>
-
-    <!-- Charts JS -->
-    <script src="assets/plugins/chart.js/chart.min.js"></script>
-
- <?php } ?>
-
+  <?php } ?>
 <?php pie() ?> <!-- Pone el fotter -->
