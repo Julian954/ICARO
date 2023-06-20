8cd1bbdedcaa8d500748f11cc81d789dec127b69
Jordan (Laptop) <miguel20010807@gmail.com>
2023-06-19 22:16:19 -0600

    CONFLICTO

--- Views/Inicio/Home.php
+++ Views/Inicio/Home.php
@@ -263,36 +263,6 @@
 							    <tbody>
                     <?php foreach ($data4 as $que) { ?>
 							      	<tr>
-<<<<<<< HEAD
-                        <td>
-                          <?php if ($validar['estado'] == 2) { ?>
-                            <svg width="0.95em" height="0.95em" viewBox="0 0 512 512" fill="blue" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M256 56c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m0-48C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 168c-44.183 0-80 35.817-80 80s35.817 80 80 80 80-35.817 80-80-35.817-80-80-80z" /></svg>
-                          <?php } elseif ($validar['estado'] == 3) { ?>
-                            <svg width="0.95em" height="0.95em" viewBox="0 0 512 512" fill="blue" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm61.8-104.4l-84.9-61.7c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v141.7l66.8 48.6c5.4 3.9 6.5 11.4 2.6 16.8L334.6 349c-3.9 5.3-11.4 6.5-16.8 2.6z" /></svg>
-                          <?php } elseif ($validar['estado'] == 4) { ?>
-                            <svg width="0.95em" height="0.95em" viewBox="0 0 512 512" fill="green" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z" /></svg>
-                          <?php } elseif ($validar['estado'] == 1) { ?>
-                            <svg width="0.95em" height="0.95em" viewBox="0 0 512 512" fill="blue" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200z" /></svg>
-                          <?php } ?>
-                        </td>
-                        <td><?php echo $validar['id_contrato']; ?></td>
-                        <td><?php echo $validar['fecha']; ?></td>
-                        <td><?php echo $validar['creador']; ?></td>
-                        <td><?php echo $validar['validador']; ?></td>
-                        <td><?php echo $validar['intentos'];?></td>
-                        <?php if ($validar['estado'] != 1) { ?>
-                          <td><a href="<?php echo base_url(); ?>Contratos/Foro?contrato=<?php echo $validar['id_contrato']; ?>">
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-right-square mr-2" fill="green" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                              <path fill-rule="evenodd" d="M5.172 10.828a.5.5 0 0 0 .707 0l4.096-4.096V9.5a.5.5 0 1 0 1 0V5.525a.5.5 0 0 0-.5-.5H6.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z"/>
-                            </svg>
-                          </a></td>
-                          <?php include('Modal_quejas.php');    ?>
-=======
                         <td><?php echo $que['descripcion']; ?></td>
                         <td><?php echo $que['piezas']; ?></td>
                         <td><?php echo $que['abreviacion']; ?></td>
@@ -302,7 +272,6 @@
                           <td>
                             <span class="badge bg-warning">Atendido</span>
                           </td>
->>>>>>> 03666988ef7674e9dad622b677f608e8edd56566
                         <?php } else { ?>
                           <td>
                             <span class="badge bg-danger">Pendiente</span>
