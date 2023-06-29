843aa5650b49300ff46e2944ee2f2181616b0fc3
Julian <sanchezjulian687@gmail.com>
2023-06-28 23:39:33 -0600

    PEDIDOS TABLE

--- ServerSide/serversidePedidos.php
+++ ServerSide/serversidePedidos.php
@@ -1,9 +1,24 @@
 <?php
 require 'serverside.php';
-$table_data->get('vista_pedidos','id',array('id', 'nopedido', 'tipo', 'clave', 'cantidad', 'eta' , 'topn', 'monto', 'fecha_alta'));
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    header("Location: modal.php");
+//Decia esta madre de tambien mandar el monto2 para comprobarlo y mostrar el boton
+$table_data->get('vista_pedido', 'id', array(
+  'id',
+  'nopedido',
+  'tipo',
+  'clave',
+  'cantidad',
+  'eta',
+  'topn',
+  'monto',
+  'fecha_alta',
+  'monto2',
+  function($row) {
+    $monto2 = $row['monto2'];
+    if ($monto2 != 0) {
+      return "<span class='label label-primary'>Enlazado</span>";
+    } else {
+      return "<div class='btn-group'><button class='btn btn-primary btn-sm btnEditarP'>ENLAZAR</button></div>";
+    }
   }
-  
+));
 ?>
