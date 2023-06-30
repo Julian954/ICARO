979a037442a6874f80e8aed132dfcb69263f3b20
Julian <sanchezjulian687@gmail.com>
2023-06-30 17:51:25 -0600

    PEDIDOS

--- ServerSide/serversidePedidos.php
+++ ServerSide/serversidePedidos.php
@@ -1,7 +1,7 @@
 <?php
 require 'serverside.php';
 //Decia esta madre de tambien mandar el monto2 para comprobarlo y mostrar el boton
-$table_data->get('vista_pedido', 'id', array(
+$table_data->get('vista_pedidos', 'id', array(
   'id',
   'nopedido',
   'tipo',
@@ -11,14 +11,5 @@ $table_data->get('vista_pedido', 'id', array(
   'topn',
   'monto',
   'fecha_alta',
-  'monto2',
-  function($row) {
-    $monto2 = $row['monto2'];
-    if ($monto2 != 0) {
-      return "<span class='label label-primary'>Enlazado</span>";
-    } else {
-      return "<div class='btn-group'><button class='btn btn-primary btn-sm btnEditarP'>ENLAZAR</button></div>";
-    }
-  }
 ));
 ?>
